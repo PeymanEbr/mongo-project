ea249d298b2af0858f7b524189066faf27109fd0
DESKTOP-G68T79M\peyman <peyman@gmail.com>
2022-12-23 12:42:35 +0330

    write schema

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/mongoproject')
 .then(() => {console.log('connected to mongodb')})
 .catch((err) => {'could not connect to mongodb'});
+
+const userScema = new mongoose.Schema({
+    first_name: String,
+    last_name: {type: String, required: true},
+    favorites: [String],
+    data: {type: Date, default: Date.now},
+    admin: Boolean,
+});
